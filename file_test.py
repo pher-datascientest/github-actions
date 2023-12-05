d8fe2eb44c7dfe6c0cb077ea85b5a0c5d49528fd
pher-datascientest <152771707+pher-datascientest@users.noreply.github.com>
2023-12-05 19:01:16 +0100

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -1,11 +1,11 @@
-import pytest
+#import pytest
 
 
 def test_calc_addition():
     # Function test the output of 2+4
     output = 2 + 4
     assert output == 6
- 
+
 
 def test_calc_substraction():
     # Function test the output of 2-4
